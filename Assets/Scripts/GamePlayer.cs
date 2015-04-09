dcb3e08f7ccac1c60fc372dc5edd56d9e9dc4a49
frankrs <frankstapleton3d@gmail.com>
2015-04-09 19:06:24 -0400

    day one, set up crude multiplay

---
+++ Assets/Scripts/GamePlayer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using Photon;
+using UnityEngine.UI;
+
+public class GamePlayer : Photon.PunBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+//	[RPC]
+	public void SentIt(){
+		photonView.RPC("ChangeColors",PhotonTargets.Others,Random.Range(0f,1f));
+	}
+
+	[RPC]
+	public void ChangeColors(float c){
+		Camera.main.backgroundColor = new Color(c,c,c,1f);
+	}
+}
---
+++ Assets/Scripts/LobbySetup.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using Photon;
+using UnityEngine.UI;
+//using ExitGames;
+
+public class LobbySetup : Photon.PunBehaviour {
+
+	public UIStuff uIStuff;
+
+	// Use this for initialization
+	void Start () {
+		PhotonNetwork.ConnectUsingSettings("1.0");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		uIStuff.connectionStateUI.text = PhotonNetwork.connectionState.ToString();
+		if(PhotonNetwork.inRoom){
+		uIStuff.room.text = PhotonNetwork.room.ToString();
+		}
+	}
+
+
+
+
+	//void OnConnectedToPhoton(){
+	void OnJoinedLobby(){
+		PhotonNetwork.JoinOrCreateRoom("playroom",null,null);
+		//PhotonNetwork.JoinLobby();
+	}
+
+	void OnJoinedRoom(){
+		PhotonNetwork.LoadLevel(1);
+	}
+
+}
+
+
+
+[System.Serializable]
+public class UIStuff {
+	public Text connectionStateUI;
+	public Text room;
+}
